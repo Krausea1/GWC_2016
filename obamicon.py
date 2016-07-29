263ff3dba9bc4a78237924747650544f182d6c60
Krausea1 <krausealexandra1@gmail.com>
2016-07-29 10:26:38 -0400

    This is my obamicon file I changed the color

---
+++ obamicon.py
@@ -0,0 +1,31 @@
+from PIL import Image
+
+darkBlue = (0, 51, 76)
+red = (217, 26, 33)
+lightBlue = (112, 150, 158)
+yellow = (252, 227, 166)
+
+im = Image.open("soa.jpg")
+data = im.getdata()
+
+data_list = list(data)
+
+data_list
+
+#length = len(data_list)
+picture_list = [] 
+for tuple in data_list:
+	total = tuple[0] + tuple[1] +tuple[2]
+	if total<182:
+		picture_list.append(darkBlue)
+	elif total > 182 and total <=364:
+		picture_list.append(red)
+	elif total>364 and total <=564:
+		picture_list.append(lightBlue)
+	else: 
+	    picture_list.append(yellow)
+	   
+new_image = Image.new("RGB", (550, 373), "white")
+new_image.putdata(picture_list)
+
+new_image.show()
